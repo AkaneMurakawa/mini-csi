6587b2c08874d18b17b67f056826959c0f288536
chenshaoj <614784818@qq.com>
2022-07-09 15:28:58 +0800

    fix string printf, line buffer

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
     char buf[1024];
-    const char prompt[3] = ">> ";
+    const char prompt[4] = ">> \0";
     printf("%s\n", "Hello, C script interpreter");
     printf("%s", prompt);
     while(fgets(buf, 1024, stdin) != NULL)
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
         str_look_ahead_token_exists = 0;
         double data = parse_expression();
         printf("%g\n", data);
+        printf("%s", prompt);
     }
     return 0;
 }
